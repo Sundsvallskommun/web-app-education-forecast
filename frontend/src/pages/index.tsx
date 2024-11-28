f47d5171637a1db98700dd61d474dcf7b707c599
Alice Fagerberg <alice.fagerberg@sundsvall.se>
2024-11-28 09:20:33 +0100

    fxes routing on index page and redirect info if wrong page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,14 +4,38 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { shallow } from 'zustand/shallow';
 import { hasRolePermission } from '@utils/has-role-permission';
+import { useForecastStore } from '@services/forecast-service/forecats-service';
+import { thisSchoolYearPeriod } from '@utils/school-year-period';
+import { QueriesDto } from '@interfaces/forecast/forecast';
 
 export default function Index() {
   const user = useUserStore((s) => s.user, shallow);
-  const { headmaster } = hasRolePermission(user);
+  const { getMyClasses } = useForecastStore();
+  const { headmaster, teacher, mentor } = hasRolePermission(user);
   const router = useRouter();
+  const { schoolYear, currentMonthPeriod, termPeriod } = thisSchoolYearPeriod();
+  const selectedSchoolYear = useForecastStore((s) => s.selectedSchoolYear);
+  const selectedPeriod = useForecastStore((s) => s.selectedPeriod);
+
+  const { GR } = hasRolePermission(user);
+  const currentPeriod = GR ? termPeriod : currentMonthPeriod;
+
+  const teacherAndMentorRoutes = () => {
+    const myGroup: QueriesDto = {
+      period: selectedPeriod ? selectedPeriod : currentPeriod,
+      schoolYear: selectedSchoolYear ? selectedSchoolYear : schoolYear,
+    };
+    if (teacher || (mentor && teacher)) {
+      router.push('/mina-amnen-grupper');
+    } else if (mentor && !teacher) {
+      getMyClasses(myGroup).then((res) => {
+        router.push(`/min-mentorsklass/${res.data[0].groupId}`);
+      });
+    }
+  };
 
   useEffect(() => {
-    headmaster ? router.push('/klasser') : router.push('/mina-amnen-grupper');
+    headmaster ? router.push('/klasser') : teacherAndMentorRoutes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <LoaderFullScreen />;
